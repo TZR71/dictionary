be95e32e29750c532463f63b8c599b797a03dab3
TZR71 <Tiinarojola@gmail.com>
2024-11-25 18:22:13 +0100

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
+import Dictionary from './Dictionary';
+
+
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-         
-      </header>
+      <div className='container'>
+        <header className="App-header"></header>
+        <main>
+          <Dictionary/>
+        </main>
+
+        <footer className='App-footer'>
+          Coded by Tiina Rojola
+        </footer>
+      </div>
     </div>
   );
 }
