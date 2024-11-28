ffe34d240bd608fd2734fd23590ebdc993c08ccb
TZR71 <Tiinarojola@gmail.com>
2024-11-28 08:39:31 +0100

    Added conditional rendering to Meanings.js and changed background image to test it

--- src/Meaning.js
+++ src/Meaning.js
@@ -10,12 +10,20 @@ export default function Meaning(props){
         <h3>{props.meaning.partOfSpeech}</h3>
         <strong>Definition:</strong>
         <p>{props.meaning.definition}</p>
-        <br />
-        <strong>Example:</strong>
-        <p>{props.meaning.example}</p>
-        <br />
-        <strong>Synonyms:</strong>
-        <Synonyms synonyms={props.meaning.synonyms} />
+       
+        {props.meaning.example && (
+          <>
+            <strong>Example:</strong>
+            <p>{props.meaning.example}</p>
+          </>
+        )}
+        
+        {props.meaning.synonyms && (
+          <>
+            <strong>Synonyms:</strong>
+            <Synonyms synonyms={props.meaning.synonyms} />
+          </>
+        )}
       </div>
     );
 }
